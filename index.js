86fd348731ce8f989bda11eaae33de61e96c7718
yasmin <yasminleticia_@hotmail.com>
2022-02-15 15:41:33 -0300

    adicionando ao git

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 require('dotenv').config();
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
-});
 
-app.listen(PORT, () => {
-    console.log('escutando');
-});
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+app.listen(port, () => console.log(`Online: ${port}`));
