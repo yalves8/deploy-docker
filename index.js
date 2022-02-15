d400c3419b3b17eba0f2f380b84dfcbc3d71abff
yasmin <yasminleticia_@hotmail.com>
2022-02-15 15:51:12 -0300

    console

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
-app.listen(port, () => console.log(`Online: ${port}`));
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log('Online: %d', port));
